95bc56ac183acb0dc47c7c6fd2685feae11b1976
Isaq <isabela.quinterom@uqvirtual.edu.co>
2025-04-03 19:41:54 -0500

    subir modelo cliente

--- src/models/cliente.java
+++ src/models/cliente.java
@@ -1,5 +1,40 @@
 package models;
 
 public class cliente {
+	private String name;
+	private String id;
+	private String placa;
+	
+	public cliente(String name, String id, String placa) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.placa = placa;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	
+	
 }
