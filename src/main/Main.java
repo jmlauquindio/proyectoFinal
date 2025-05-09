3715fd93a177eef2c741a4da1087fd248a4db93a
Jorge Mario <autopartsjypweb@gmail.com>
2025-05-08 19:06:11 -0500

    Mejora en interfaz para utilizar HTML

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,5 @@
 package main;
+import javax.swing.*;
 
 import controllers.Parqueadero;
 
@@ -9,8 +10,21 @@ public class Main {
          
         parqueadero.ingresarCliente("Juan Pérez", "12345", 300123456, "juanperez@mail.com");
         parqueadero.ingresarCliente("Ana Gómez", "67890", 300987654, "anagomez@mail.com");
-         
-        System.out.println("Lista de Clientes:");
-        parqueadero.getlistaClientes();
+          
+        //parqueadero.getlistaClientes();
+        listaClientes(parqueadero);
+    }
+    public static void listaClientes(Parqueadero parqueadero) {
+
+        SwingUtilities.invokeLater(() -> {
+            String html = "<html><h2 style='background:#CDCDCD;color:white;padding:30px;'>Listado de clientes:</h2>"; 
+            html = html + "<p>" + parqueadero.getListaClientesComoTexto() + "</p></html>";
+
+            JEditorPane editorPane = new JEditorPane("text/html", html);
+            editorPane.setEditable(false);
+            editorPane.setOpaque(false);
+
+            JOptionPane.showMessageDialog(null, editorPane, "Lista de clientes", JOptionPane.INFORMATION_MESSAGE);
+        });
     }
 }
