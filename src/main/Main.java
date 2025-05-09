5f0737741de668a5c4e32c6620ba062f424a6847
Jorge Mario <autopartsjypweb@gmail.com>
2025-05-08 19:56:37 -0500

    Mejoras de intefaz y pruebas

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 
 import controllers.Parqueadero;
 
@@ -17,14 +19,39 @@ public class Main {
     public static void listaClientes(Parqueadero parqueadero) {
 
         SwingUtilities.invokeLater(() -> {
-            String html = "<html><h2 style='background:#CDCDCD;color:white;padding:30px;'>Listado de clientes:</h2>"; 
-            html = html + "<p>" + parqueadero.getListaClientesComoTexto() + "</p></html>";
+            String html = "<html style='height:100px;overflow:scroll'>"
+                    + "<h2 style='width:500px;background:#CDCDCD;color:white;padding:0 30px;'>Listado de clientes:</h2>"
+                    + "<p>" + parqueadero.getListaClientesComoTexto() + "</p>"
+                    + "<img style='width:100px' src='https://www.pngall.com/wp-content/uploads/15/User-PNG-Images-HD-180x180.png'/>"
+                    + "<p><a href='accion:exportar'><span style='background-color:yellow;width: 200px;'>[Exportar a archivo]</span></a></p>"
+                    + "</html>";
 
             JEditorPane editorPane = new JEditorPane("text/html", html);
             editorPane.setEditable(false);
             editorPane.setOpaque(false);
 
-            JOptionPane.showMessageDialog(null, editorPane, "Lista de clientes", JOptionPane.INFORMATION_MESSAGE);
+
+            JScrollPane scrollPane = new JScrollPane(editorPane);
+            scrollPane.setPreferredSize(new java.awt.Dimension(500, 400)); 
+            
+            editorPane.addHyperlinkListener(new HyperlinkListener() {
+                @Override
+                public void hyperlinkUpdate(HyperlinkEvent e) {
+                    if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                        if ("accion:exportar".equals(e.getDescription())) {
+                            exportarClientes(parqueadero);
+                        }
+                    }
+                }
+            }); 
+
+            
+            JOptionPane.showMessageDialog(null, editorPane, "Lista de clientes", JOptionPane.PLAIN_MESSAGE);
         });
+    }
+
+    public static void exportarClientes(Parqueadero parqueadero) {
+        System.out.println("Exportando clientes...");
+        JOptionPane.showMessageDialog(null, "Clientes exportados correctamente.");
     }
 }
